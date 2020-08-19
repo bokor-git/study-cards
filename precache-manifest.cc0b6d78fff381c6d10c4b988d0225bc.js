16ae7173b1a761cb4c31468e015aee0be7dc7a77
bokor-git <bogdankorolbk@gmail.com>
2020-08-19 13:13:24 +0300

    Updates

---
+++ precache-manifest.cc0b6d78fff381c6d10c4b988d0225bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31aaca21bd2338040b357bb2dc589963",
+    "url": "/study-cards/index.html"
+  },
+  {
+    "revision": "bbe008957e8c29a991c4",
+    "url": "/study-cards/static/css/main.124b2fe3.chunk.css"
+  },
+  {
+    "revision": "b5a70a862c2e31eb77ce",
+    "url": "/study-cards/static/js/2.5d93973c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/study-cards/static/js/2.5d93973c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe008957e8c29a991c4",
+    "url": "/study-cards/static/js/main.ac64e8bc.chunk.js"
+  },
+  {
+    "revision": "54237901025c91448855",
+    "url": "/study-cards/static/js/runtime-main.e4777a31.js"
+  }
+]);
