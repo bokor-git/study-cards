031d1cd1cfbda6bc0ebbcd6195d51161cbaa1a96
bokor-git <bogdankorolbk@gmail.com>
2020-08-19 12:32:54 +0300

    preparation

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './main/m1-ui/App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import {store} from "./main/m2-bll/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/main/m1-ui/App.tsx
+++ src/main/m1-ui/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import {HashRouter, Switch, Route} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import PasswordResetPage from "./pages/PasswordResetPage";
+import PasswordGenerationPage from "./pages/PasswordGenerationPage";
+import ProfilePage from "./pages/ProfilePage";
+import HeaderNavbar from "./header/HeaderNavbar";
+
 
 function App() {
     return (
+        <HashRouter>
             <div className="App">
+                <HeaderNavbar/>
+                <>
+                    <Switch>
+                        <Route exact path={"/"} render={() => (<LoginPage/>)}/>
+                        <Route path={"/login/"} render={() => (<LoginPage/>)}/>
+                        <Route path="/registration" render={() => (<RegistrationPage/>)}/>
+                        <Route path="/password-reset" render={() => (<PasswordResetPage/>)}/>}
+                        <Route path="/password-generation" render={() => (<PasswordGenerationPage/>)}/>
+                        <Route path="/profile" render={() => (<ProfilePage/>)}/>
+                    </Switch>
+                </>
             </div>
+        </HashRouter>
     );
 }
 
