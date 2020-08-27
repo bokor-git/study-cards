a8303b7c86c4e5d104f8ca21e82e988378dd9b0f
Keizak <keizak@yandex.ru>
2020-08-27 20:25:25 +0300

    Update design RegisterPage 08/27

--- src/main/m2-bll/registration-reducer.ts
+++ src/main/m2-bll/registration-reducer.ts
@@ -1,6 +1,6 @@
 import {handleServerNetworkError} from "../m1-ui/utils/error-utils";
 import {Dispatch} from "redux";
-import {SetAppErrorActionType, SetAppStatusActionType} from "./app-reducer";
+import {setAppErrorAC, SetAppErrorActionType, SetAppStatusActionType} from "./app-reducer";
 import {registrationApi, RegistretionDataType} from "../m3-dal/register-api";
 
 const initialState: InitialStateType = {
@@ -24,22 +24,24 @@ export const registrationReducer = (state = initialState, action: ActionsType) =
 
 // thunks
 export const registrationTC = (data: RegistretionDataType) => (dispatch: ThunkDispatch) => {
-    dispatch(setIsLoadingAC(true))
     registrationApi.setRegister(data)
         .then(res => {
             dispatch(setIsRegistratedAC(true))
             dispatch(setIsLoadingAC(false))
         }).catch((error) => {
+        dispatch(setIsLoadingAC(false))
         handleServerNetworkError(error, dispatch);
+        setTimeout(() => dispatch(setAppErrorAC(null)), 3000);
+
     })
 }
 
 
 // Action Creators
 export const setIsRegistratedAC = (value: boolean): setIsRegistratedActionType =>
-    ({type: 'SET-IS-REGISTATED', value} as const)
+    ({type: 'SET-IS-REGISTRATED', value} as const)
 export const setIsLoadingAC = (value: boolean): setIsLoadingActionType =>
-    ({type: 'SET-IS-REGISTATED', value} as const)
+    ({type: 'SET-IS-LOADING', value} as const)
 
 
 // Types
